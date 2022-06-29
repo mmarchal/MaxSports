fa9c566a482d0a4b38bb2a565aacfc63e9602e54
Maxime <marchal.maxime@live.fr>
2022-06-29 18:47:38 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:max_sports/firebase_options.dart';
 import 'package:max_sports/home.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
